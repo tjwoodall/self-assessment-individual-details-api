94b8b8e91911f246d1ce00f4eb59a02f7efd3353
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2023-06-01 13:46:29 +0000

    'Creating new service self-assessment-individual-details-api'

---
+++ app/uk/gov/hmrc/selfassessmentindividualdetailsapi/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.selfassessmentindividualdetailsapi.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
