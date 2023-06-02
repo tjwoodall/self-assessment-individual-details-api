ea148eac0aa8b151e06121a53beb299610614ca3
Samuel Jacobs <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2023-06-02 11:29:16 +0100

    MTDSA-15109: Added compile app dependencies and basic resources

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,18 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.15.0"
-  
+  private val bootstrapPlayVersion = "7.15.0"
 
-  val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion
+  val compile: Seq[ModuleID] = Seq(
+    ws,
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
+    "org.typelevel"                %% "cats-core"                 % "2.9.0",
+    "com.chuusai"                  %% "shapeless"                 % "2.4.0-M1",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.14.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion            % "test, it",
-    
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapPlayVersion % "test, it"
   )
+
 }
