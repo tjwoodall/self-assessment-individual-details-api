7324cecbee2db20d51bcc7d16da0216460b4f138
Behrouz Karimi <125777824+bkarimii@users.noreply.github.com>
2026-06-08 10:18:28 +0100

    MTDSA-34694 - Fix vulnerabilities in Self Assessment Individual Details API by upgrading Handlebars (#79)
    * MTDSA-34694
    
    * MTDSA-34694

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "org.typelevel"                %% "cats-core"                 % "2.13.0",
-    "com.github.jknack"             % "handlebars"                % "4.3.1"
+    "com.github.jknack"             % "handlebars"                % "4.5.1"
   )
 
   val test: Seq[sbt.ModuleID] = Seq(
@@ -33,8 +33,8 @@ object AppDependencies {
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = Seq(
-    "io.swagger.parser.v3"          % "swagger-parser-v3"    % "2.1.39",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.21.2"
+    "io.swagger.parser.v3"          % "swagger-parser-v3"    % "2.1.43",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.22.0"
   ).map(_ % Test)
 
 }
